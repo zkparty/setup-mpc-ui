99dab6e11671fb5d66932f928b3d449dc2e73c3a
Ivan Chub <ivansergchub@gmail.com>
2020-01-16 00:39:26 -0800

    adding router and link to ceremony page

--- index.tsx
+++ index.tsx
@@ -1,9 +1,44 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  HashRouter
+} from "react-router-dom";
 
 const App = () => {
-  return <div>this is the app</div>;
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/about"></Route>
+        <Route path="/ceremony">
+          <CeremonyPage></CeremonyPage>
+        </Route>
+        <Route exact path="/">
+          <LandingPage></LandingPage>
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
 };
+
+const LandingPage = () => {
+  return (
+    <div>
+      ZK Party
+      <br />
+      <Link to="/ceremony"> ceremony</Link>
+    </div>
+  );
+};
+
+const CeremonyPage = () => {
+  return <div>This is the ceremony page</div>;
+};
+
+const CeremonySummary = () => {};
 
 const div = document.createElement("div");
 document.body.appendChild(div);
