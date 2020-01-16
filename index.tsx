2209c85807a62c9f72999931e1a7e0291b8fe595
Ivan Chub <ivansergchub@gmail.com>
2020-01-16 01:07:26 -0800

    fleshing out the styling of the frontend

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ import {
   Link,
   HashRouter
 } from "react-router-dom";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 interface Ceremony {
   title: string;
@@ -18,31 +18,33 @@ interface Ceremony {
 
 const ceremonies: Ceremony[] = [
   {
-    title: "test ceremony",
+    title: "goblin",
+    description:
+      "Ingredients. 1 1/2 cups (355 ml) warm water (105°F-115°F) 1 package (2 1/4 teaspoons) of active dry yeast. 3 3/4 cups (490 g) bread flour. 2 tablespoons extra virgin olive oil (omit if cooking pizza in a wood-fired pizza oven) 2 teaspoons salt. 1 teaspoon sugar.",
+    start: "2020-01-16",
+    end: "2020-01-17"
+  },
+  {
+    title: "this is a test ceremony",
+    description:
+      "noun a flowerless plant which has feathery or leafy fronds and reproduces by spores released from the undersides of the fronds. Ferns have a vascular system for the transport of water and nutrients.",
+    start: "2020-01-16",
+    end: "2020-01-17"
+  },
+  {
+    title: "pandas are kinda the best",
     description: "test description",
     start: "2020-01-16",
     end: "2020-01-17"
   },
   {
-    title: "test ceremony",
+    title: "zk snark test ceremony",
     description: "test description",
     start: "2020-01-16",
     end: "2020-01-17"
   },
   {
-    title: "test ceremony",
-    description: "test description",
-    start: "2020-01-16",
-    end: "2020-01-17"
-  },
-  {
-    title: "test ceremony",
-    description: "test description",
-    start: "2020-01-16",
-    end: "2020-01-17"
-  },
-  {
-    title: "test ceremony",
+    title: "fluffy shmufy",
     description: "test description",
     start: "2020-01-16",
     end: "2020-01-17"
@@ -52,6 +54,7 @@ const ceremonies: Ceremony[] = [
 const App = () => {
   return (
     <HashRouter>
+      <GlobalStyle />
       <Switch>
         <Route exact path="/coordinators">
           <CoordinatorPage />
@@ -71,12 +74,9 @@ const LandingPage = () => {
   return (
     <LandingPageContainer>
       <LandingPageTitle>ZK Party</LandingPageTitle>
-      <br />
-      <br />
       {ceremonies.map(c => (
         <CeremonySummary ceremony={c} />
       ))}
-      <Link to="/ceremony"> ceremony</Link>
     </LandingPageContainer>
   );
 };
@@ -106,14 +106,14 @@ const CeremonySummary = (props: { ceremony: Ceremony }) => {
 
   return (
     <CeremonyContiner>
-      {c.title} <br />
+      <CeremonyLinkTitle>{c.title}</CeremonyLinkTitle>
       {c.description} <br />
+      <Link to="/ceremony"> ceremony</Link>
     </CeremonyContiner>
   );
 };
 
 const LandingPageContainer = styled.div`
-  padding-top: 48px;
   display: flex;
   width: 100vw;
   flex-direction: column;
@@ -124,16 +124,34 @@ const LandingPageContainer = styled.div`
 const LinksContainer = styled.div``;
 
 const LandingPageTitle = styled.div`
-  font-size: 30pt;
+  font-size: 50pt;
+  margin-bottom: 32px;
 `;
 
 const CeremonyContiner = styled.div`
-  background-color: green;
+  background-color: #eee;
   margin: 10px;
   width: 512px;
   padding: 16px;
+  border-radius: 4px;
+  cursor: pointer;
 `;
 
+const CeremonyLinkTitle = styled.div`
+  font-size: 18pt;
+  margin-bottom: 16px;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #fefefe;
+    color: #222;
+    margin: 0;
+    margin-top: 64px;
+    margin-bottom: 64px;
+    font-family: 'Inconsolata', monospace;
+  }
+`;
 const div = document.createElement("div");
 document.body.appendChild(div);
 ReactDOM.render(<App></App>, div);
