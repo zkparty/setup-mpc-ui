34eccbb055a377768d8bbe5b8ca7b3b54dcdfe2d
Geoff Lamperd <glamperd@gmail.com>
2020-11-20 12:12:07 +1100

    compute zkey

--- server/CircuitHandler.js
+++ server/CircuitHandler.js
@@ -8,14 +8,45 @@ const fbSkey = require("./firebase_skey.json");
 const { updateFBCeremony } = require("./FirebaseApi");
 
 var logCatcher = [];
-Logger.useDefaults();
-Logger.setHandler((messages, context) => {
+const captureHandler = (messages, context) => {
     if (context.level === Logger.INFO) {
         logCatcher.push(messages[0]);
     }
+};
+const consoleHandler = Logger.createDefaultHandler();
+Logger.useDefaults();
+Logger.setHandler((m, c) => {
+    captureHandler(m,c);
+    consoleHandler(m,c);
 });
+const catchLogger = Logger.get("catcher");
+const consoleLogger = Logger.get("console");
 
-const storage = new Storage({keyFilename: '/home/geoff/setup-mpc-ui/server/firebase_skey.json', projectId: fbSkey.project_id });
+const storage = new Storage({keyFilename: `${process.cwd()}/firebase_skey.json`, projectId: fbSkey.project_id });
+
+const POT_FILE_PATTERN='pot%EXP%_final.ptau';
+
+const checkAndDownloadFromStorage = async (prefix, fileNameFilter, deriveLocalPath) => {
+    //console.log(`project id ${fbSkey.project_id}`);
+    const [files] = await storage.bucket(`${fbSkey.project_id}.appspot.com`).getFiles({
+        prefix: prefix, 
+        delimiter: '/'
+    });
+    //console.log('Files:');
+    const matchFiles = files.filter(file => fileNameFilter(file.name));
+    if (matchFiles.length > 0) {
+        console.log(`found matching file ${matchFiles[0].name}`);
+        const destFile = deriveLocalPath(matchFiles[0].name);
+        const file = await matchFiles[0].download({
+            destination: destFile,
+        });
+        console.log(`Downloaded ${destFile}`);
+        return destFile;
+    } else {
+        console.log(`no matching file found.`);
+        return false;
+    }
+};
 
 async function getCircuitInfo(ceremonyId) {
     await openR1csFile(ceremonyId);
@@ -23,25 +54,17 @@ async function getCircuitInfo(ceremonyId) {
 };
 
 async function openR1csFile(ceremonyId) {
-    console.log(`project id ${fbSkey.project_id}`);
-    const [files] = await storage.bucket(`${fbSkey.project_id}.appspot.com`).getFiles({
-        prefix: 'ceremony_data/', 
-        delimiter: '/'
-    });
-    //console.log('Files:');
-    const r1csFiles = files.filter(file => {
-        return file.name.toLowerCase().endsWith('.r1cs');
-    });
-    if (r1csFiles.length > 0) {
-        console.log(`found R1CS file ${r1csFiles[0].name}`);
-        const destFile = `./data/${r1csFiles[0].name}`;
-        const file = await r1csFiles[0].download({
-            destination: destFile,
-        });
-        console.log(`Downloaded ${destFile}`);
+    const r1csFile = await checkAndDownloadFromStorage(
+        `ceremony_data/${ceremonyId}/`,
+        filename => filename.toLowerCase().endsWith('.r1cs'),
+        filename => `./data/${filename}`
+    );
+    if (r1csFile) {
+        console.log(`Downloaded ${r1csFile}`);
         // Get circuit info
         logCatcher = [];
-        await snarkjs.r1cs.info(destFile, Logger);
+        // TODO - make snarkjs return a JSON object
+        await snarkjs.r1cs.info(r1csFile, catchLogger);
         var numConstraints = 0;
         logCatcher.forEach(m => {
             console.log(`m: ${m}`);
@@ -60,6 +83,31 @@ async function openR1csFile(ceremonyId) {
         };
         await updateFBCeremony(ceremonyUpdate);
         console.log('Ceremony updated');
+
+        // Prepare zkey file
+        const potFile = POT_FILE_PATTERN.replace("%EXP%", powers);
+        console.log(`PoT file: ${potFile}`);
+        const potPath = `./data/ptau/${potFile}`;
+        // See if we have the file locally
+        fs.stat(potPath, async (err, stat) => {
+            if (err == null) {
+                console.log('Already have PoT file');
+            } else if (err.code === 'ENOENT') {
+                // Look for it on storage, then download it.
+                const potFileLocal = await checkAndDownloadFromStorage(
+                    'ptau/',
+                    filename => filename.endsWith(potFile),
+                    () => potPath
+                );
+                // TODO - put these status messages in a status field on tghe FB ceremony doc
+                if (!potFileLocal) throw `Couldn't find or download download PoT file`;
+            } else throw err;
+
+            // Have PoT & r1cs files. Now make the zkey file.
+            const zkeyFile = `./data/ceremony_data/${ceremonyId}/ph2_0000.zkey`;
+            await snarkjs.zKey.newZKey(r1csFile, potPath, zkeyFile, consoleLogger);
+            console.log(`Zkey file generated: ${zkeyFile}`);
+        });
     } else {
         console.log(`no R1CS file found for ${ceremonyId}.`);        
     }
