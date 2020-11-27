231c86fc0ee56f16719280352493621b58047f89
Geoff Lamperd <glamperd@gmail.com>
2020-11-27 19:35:27 +1100

    spinner for calculation

--- client/src/app/ParticipantPage.tsx
+++ client/src/app/ParticipantPage.tsx
@@ -20,44 +20,58 @@ import {
 } from "../api/ZKPartyApi";
 import { Ceremony } from "../types/ceremony";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const Loaded = ({ wasm }: { wasm: any}) => (<Button onClick={wasm.contribute} style={{color: 'white'}}>Contribute</Button>);
+const Loaded = ({ wasm, contribute }: { wasm: any, contribute: () => any}) => (<Button onClick={contribute} style={{color: 'white'}}>Contribute</Button>);
 
 const Unloaded = (props: { loading: boolean, loadWasm: any }) => {
   return props.loading ? (
     <div>Loading...</div>
   ) : (
-    <button onClick={props.loadWasm}>Load library</button>
+    <Button onClick={props.loadWasm} style={{color: 'white'}}>Load library</Button>
   );
 };
 
 export const ParticipantSection = () => {
   const [loading, setLoading] = React.useState(false);
   const [wasm, setWasm] = React.useState<any | null>(null);
+  const [data, setData] = React.useState<Uint8Array | null>(null);
+  const [running, setRunning] = React.useState(false);
 
   const loadWasm = async () => {
     try {
       setLoading(true);
+      // ignore syntax check error. Make sure to *npm link* phase2 in ../lib/pkg
       const wasm = await import('phase2');
       setWasm(wasm);
-    } finally {
+      let paramData: any = await fetch('/zk_transaction_1_2.params');
+      paramData = await paramData.arrayBuffer();
+      paramData = new Uint8Array(paramData);
+      console.log('Source params', paramData);
+      setData(paramData);
+      } finally {
       setLoading(false);
     }
   };
 
   const run = () => {
+    setRunning(true);
+    const result = wasm.contribute(data);
+    console.log('Updated params', result)
+    setRunning(false);
   };
 
   return (
     <div style={{ width: "512px" }}>
       <Typography variant="body1">
-        Welcome to zkparty. This page will allow you to participate in the ceremony. Once you agree, your computation will commence. 
+        Welcome to zkparty. This page will allow you to participate in the ceremony. Click to your commence your contribution. 
       </Typography>
-      {wasm ? (
-          <Loaded wasm={wasm} />
+      {running ? (<><CircularProgress /><Typography>Calculating ...</Typography></>) : (
+        wasm && data ? (
+          <Loaded wasm={wasm} contribute={run} />
         ) : (
           <Unloaded loading={loading} loadWasm={loadWasm} />
-        )}
+        ))}
     </div>
   );
 };
