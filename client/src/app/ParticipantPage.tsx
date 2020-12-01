7c400aa31f46cb24f98d4c001b4be52cce1d55c0
Geoff Lamperd <glamperd@gmail.com>
2020-12-01 19:49:05 +1100

    compute in promise

--- client/src/app/ParticipantPage.tsx
+++ client/src/app/ParticipantPage.tsx
@@ -18,9 +18,10 @@ import {
   getCeremonySummaries,
   getCeremonySummariesCached
 } from "../api/ZKPartyApi";
-import { Ceremony } from "../types/ceremony";
+import { Ceremony, CeremonyEvent } from "../types/ceremony";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { addCeremonyEvent } from "../api/FirebaseApi";
 
 const Loaded = ({ wasm, contribute }: { wasm: any, contribute: () => any}) => (<Button onClick={contribute} style={{color: 'white'}}>Contribute</Button>);
 
@@ -32,14 +33,53 @@ const Unloaded = (props: { loading: boolean, loadWasm: any }) => {
   );
 };
 
+const CreateCeremonyEvent = (eventType: string, message: string): CeremonyEvent => {
+  return {
+    sender: "PARTICIPANT",
+    eventType,
+    timestamp: new Date(),
+    message,
+    acknowledged: false,  
+  };
+}
+
+interface ComputeStatus {
+  running: boolean,
+  downloaded: boolean,
+  started: boolean,
+  computed: boolean,
+  newParams: Uint8Array,
+  uploaded: boolean,
+};
+
+const initialComputeStatus: ComputeStatus = {
+  running: false,
+  downloaded: false,
+  started: false,
+  computed: false,
+  newParams: new Uint8Array(),
+  uploaded: false,
+}
+
+const DoComputation = (wasm: any, params: Uint8Array, entropy: Buffer) => new Promise<Uint8Array>((resolve, reject) => {
+  try {
+    const newParams = wasm.contribute(params, entropy);
+    console.log('Updated params', newParams);
+    resolve(newParams);
+  } catch (err) {
+    reject(err);
+  }
+});
+
 export const ParticipantSection = () => {
   const [loading, setLoading] = React.useState(false);
   const [wasm, setWasm] = React.useState<any | null>(null);
   const [data, setData] = React.useState<Uint8Array | null>(null);
-  const [running, setRunning] = React.useState(false);
   const [entropy, setEntropy] = React.useState(new Uint8Array(64));
-  const ceremonyId = "E2F5wqJvxbKVcl9Mv2aD";
-  let index: number = 1;
+  const [computeStatus, setComputeStatus] = React.useState<ComputeStatus>(initialComputeStatus);
+  //const [index, setIndex] = React.useState(1);
+  const ceremonyId = "Qiu6eLsJDyMokvJ7sGzH";
+  const index = 1;
 
   const getEntropy = () => {
     const s = entropy.map((v, i) => Math.random() * 256);
@@ -54,10 +94,8 @@ export const ParticipantSection = () => {
       // ignore syntax check error. Make sure to *npm link* phase2 in ../lib/pkg
       const wasm = await import('phase2');
       setWasm(wasm);
-      //let paramData: any = await fetch('/zk_transaction_1_2.params');
-      //paramData = await paramData.arrayBuffer();
-      //paramData = new Uint8Array(paramData);
       const paramData = await GetParamsFile(ceremonyId, index);
+      addCeremonyEvent(ceremonyId, CreateCeremonyEvent( "PARAMS_DOWNLOADED", `Parameters from participant ${index} downloaded OK`));
       console.log('Source params', paramData);
       setData(paramData);
       getEntropy();
@@ -66,41 +104,65 @@ export const ParticipantSection = () => {
     }
   };
 
-  const compute = () => {
+  const compute = async () => {
+    const { running, downloaded, started, computed, uploaded, newParams } = computeStatus;
+    console.log(`compute step: ${running? 'running' : '-'} ${running && downloaded && !computed ? 'computing': running && computed && !uploaded ? 'uploaded' : 'inactive'}`);
     if (running) {
-      console.log('running computation...');
-      const result = wasm.contribute(data, Buffer.from(getEntropy()));
-      console.log('Updated params', result)
-      const paramsFile = UploadParams(ceremonyId, ++index, result);
-      // Add event to notify status and params file name
-      setRunning(false);
+      if (!downloaded) {
+        setComputeStatus({...computeStatus, downloaded: true});
+      }
+      if (downloaded && !computed) {
+        if (!started) {
+          console.log('running computation......');
+          if (data) {
+            DoComputation(wasm, data, Buffer.from(getEntropy())).then(async (newParams) => {
+              console.log('DoComputation finished');
+              await addCeremonyEvent(ceremonyId, CreateCeremonyEvent(
+                "COMPUTE_CONTRIBUTION", 
+                `Contribution for participant ${index + 1} completed OK`
+              ));
+              setComputeStatus({...computeStatus, computed: true, newParams});
+          })};
+          setComputeStatus({...computeStatus, started: true});
+        }
+      }
+      if (computed && !uploaded) {
+        const newIndex = index+1;
+        const paramsFile = await UploadParams(ceremonyId, newIndex, newParams);
+        // Add event to notify status and params file name
+        await addCeremonyEvent(ceremonyId, CreateCeremonyEvent(
+          "PARAMS_UPLOADED", 
+          `Parameters for participant ${newIndex} uploaded to ${paramsFile}`
+        ));
+        setComputeStatus({...computeStatus, running: false, uploaded: true, newParams: new Uint8Array()});
+      }
     }
   };
 
   compute();
 
   const run = () => {
-    setRunning(true);
+    setComputeStatus({...initialComputeStatus, running: true });
   };
 
-  const serviceWorker = () => { 
-    navigator.serviceWorker.ready.then(() => {
-      console.log('service worker ready');
-      navigator.serviceWorker.controller?.postMessage({type: 'LOAD_WASM'});
-      navigator.serviceWorker.addEventListener('message', event => {
-        console.log(`message from service worker ${event.data.type}`);
-      });
-    });
-  };
+  // const serviceWorker = () => { 
+  //   navigator.serviceWorker.ready.then(() => {
+  //     console.log('service worker ready');
+  //     navigator.serviceWorker.controller?.postMessage({type: 'LOAD_WASM'});
+  //     navigator.serviceWorker.addEventListener('message', event => {
+  //       console.log(`message from service worker ${event.data.type}`);
+  //     });
+  //   });
+  // };
 
-  serviceWorker();
+  //serviceWorker();
 
   return (
     <div style={{ width: "512px" }}>
       <Typography variant="body1">
         Welcome to zkparty. This page will allow you to participate in the ceremony. Click to your commence your contribution. 
       </Typography>
-      {running ? (<><CircularProgress /><Typography>Calculating ...</Typography></>) : (
+      {computeStatus.running ? (<><CircularProgress /><Typography>{computeStatus.downloaded ? computeStatus.computed ? 'Uploading' : 'Calculating' : 'Preparing'} ...</Typography></>) : (
         wasm && data ? (
           <Loaded wasm={wasm} contribute={run} />
         ) : (
