8c4a0b9c5a656e78c352a70978e098c9f8caf68a
Geoff Lamperd <glamperd@gmail.com>
2020-12-11 08:17:29 +1100

    update INVALIDATED

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions';
+
 // import { Ceremony, CeremonyEvent, CeremonyState, 
 //     Contribution, ContributionState, ContributionSummary, 
 //     Participant, ParticipantState } from './types/ceremony';
-//import firebase from 'firebase/app';
-//import "firebase/firestore";
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import firebaseConfig from './firebaseConfig';
 //import { jsonToCeremony, jsonToContribution } from './ZKPartyApi';
   
 // // Start writing Firebase Functions
@@ -35,6 +37,9 @@ import * as functions from 'firebase-functions';
 //     }
 // }
 
+firebase.initializeApp(firebaseConfig);
+
+
 export const ContributionWatchdog = functions.firestore
   .document('ceremonies/{cId}/contributions/{contribId}')
   .onUpdate((change, context) => { 
@@ -42,6 +47,7 @@ export const ContributionWatchdog = functions.firestore
         const contribId = context.params.contribId;
         // Start timeout timer
         functions.logger.info(`contribution status for ${contribId} is now ${change.after.data().status}`);
+        // submit pubsub scheduled task
       } else if (change.after.data().status === 'COMPLETE') {
         functions.logger.info(`contribution ${context.params.contribId} is COMPLETE`);
 
@@ -49,6 +55,42 @@ export const ContributionWatchdog = functions.firestore
       return new Promise((resolve) => {
         setTimeout(() => {
           resolve('done');
-        }, 20000);
+        }, 200);
       });
    }); 
+
+export const TimeoutWatchdog = functions.pubsub.schedule('every 3 minutes').onRun(async (context) => {
+  functions.logger.debug(`pubsub check ${context.eventType}`);
+  const db = firebase.firestore();
+  const snap = await db
+    .collectionGroup("contributions")
+    .where('status', '==', 'RUNNING')
+    .get();
+
+    snap.forEach(async contrib => {
+      functions.logger.debug(`contribution ${contrib.id} is running`);
+      const ceremony = contrib.ref.parent ? contrib.ref.parent.parent : undefined;
+      functions.logger.debug(`ceremony id ${ceremony?.id}`);
+      if (ceremony) {
+        const events = await 
+          ceremony
+          .collection('events')
+          .orderBy('timestamp', 'desc')
+          .limit(1)
+          .get();
+        let lastSeen = 0;
+        if (!events.empty) {
+          lastSeen = events.docs[0].get('timestamp')/1000;
+        };
+        functions.logger.debug(`age ${(Date.now() - lastSeen)/1000} s`);
+        if ((Date.now() - lastSeen) > 300000) {
+          functions.logger.info(`expired contribution ${contrib.id}`);
+          await contrib.ref.update({'status': 'INVALIDATED'});
+          // add event
+        }
+      }
+    });
+    //  } else {
+    //    functions.logger.debug('no running contributions');
+    //  }
+});
