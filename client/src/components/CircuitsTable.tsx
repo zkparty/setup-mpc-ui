dad795bc39f79c5e9024ceaa09ed33c065aa700c
Geoff Lamperd <glamperd@gmail.com>
2021-03-11 02:56:21 +1100

    table styles

--- client/src/components/CircuitsTable.tsx
+++ client/src/components/CircuitsTable.tsx
@@ -7,7 +7,8 @@ import {
     PageContainer,
     lighterBackground,
     SectionContainer,
-    CeremonyTitle
+    CeremonyTitle,
+    darkBorder,
   } from "../styles";
 //import './styles.css';
 import { Ceremony } from "../types/ceremony";
@@ -18,22 +19,34 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { Button, makeStyles, Typography } from '@material-ui/core';
+import { Button, withStyles, Typography } from '@material-ui/core';
 import { ComputeDispatchContext, ComputeStateContext } from '../state/ComputeStateManager';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: '1092px',
+const ceremonyProject = 'zkopru';
+
+const StyledTable = withStyles({
+  root: {
     color: textColor,
     backgroundColor: lighterBackground,
   },
-});
+})(Table);
+
+const StyledCell = withStyles({
+  head: {
+    color: textColor,
+  },
+  root: {
+    color: textColor,
+    height: '53px',
+    border: `1px solid ${darkBorder}`,
+  }
+})(TableCell);
 
 export default function CircuitsTable(props: any) {
   const state = useContext(ComputeStateContext);
   const dispatch = useContext(ComputeDispatchContext);
   const [loaded, setLoaded] = useState(false);
-  const classes = useStyles();
+  //const classes = useStyles();
   console.debug(`render circuits table`);
 
   const { circuits } = state;
@@ -63,30 +76,52 @@ export default function CircuitsTable(props: any) {
 
   return (
     <TableContainer component='div'>
-      <Typography variant='h2' style={{ font: 'Inconsolata', fontSize: '48px' , color: '#9f8fff' }}>Circuit Table</Typography>
-      <Table className={classes.table} size="small" aria-label="circuits table">
+      <Typography variant='h2' style={{
+         fontFamily: 'Inconsolata',
+         fontWeight: 'bold', 
+         fontSize: '48px' , 
+         color: textColor, 
+         height: '100px',
+         paddingBottom: '6px',
+      }}>
+        {`${ceremonyProject} circuits`}
+      </Typography>
+      <Typography variant='body1' style={{ 
+        fontFamily: 'Inconsolata', 
+        fontSize: '18px', 
+        fontWeight: 'normal',
+        color: textColor, 
+        paddingBottom: '64px',
+        }}>
+        {`All participants will contribute a computation to ${circuits.length} different circuits. There is no limit
+        to the number of contributions each circuit can accept - The more the merrier! 
+        Participants receive a hash for each completed circuit, which acts as a signature of 
+        their contribution`}
+      </Typography>
+      <StyledTable size="small" aria-label="circuits table" >
         <TableHead>
           <TableRow>
-            <TableCell>Circuit</TableCell>
-            <TableCell align="right">Contributions</TableCell>
-            <TableCell align="right">Average Time</TableCell>
-            <TableCell align="right">Transcript</TableCell>
+            <StyledCell style={{ width: '105px' }}>Circuit</StyledCell>
+            <StyledCell style={{ width: '167px' }} align="center">Contributions</StyledCell>
+            <StyledCell style={{ width: '156px' }} align="center">Average Time</StyledCell>
+            <StyledCell style={{ width: '157px' }} align="center">Transcript</StyledCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {circuits.map((circuit, index) => (
             <TableRow key={index+1}>
-              <TableCell component="th" scope="row">
+              <StyledCell align='left' >{index+1}</StyledCell>
+              <StyledCell component="th" scope="row" align='left' >
                 {circuit.numParticipants}
-              </TableCell>
-              <TableCell align="right">{circuit.averageDuration}</TableCell>
-              <TableCell align="right">
-                <Button onClick={() => showTranscript(circuit)}>View</Button>
-              </TableCell>
+              </StyledCell>
+              <StyledCell align="left">{circuit.averageDuration}</StyledCell>
+              <StyledCell align="center">
+                <Button style={{ color: textColor }} onClick={() => showTranscript(circuit)}>View</Button>
+              </StyledCell>
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </StyledTable>
     </TableContainer>    
     )
   };
