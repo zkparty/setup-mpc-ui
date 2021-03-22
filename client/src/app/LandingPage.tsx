0a1c1353f71e42d9bb8a88d35c7f88001e496df9
Geoff Lamperd <glamperd@gmail.com>
2021-03-22 12:58:14 +1100

    refactor landing page

--- client/src/app/LandingPage.tsx
+++ client/src/app/LandingPage.tsx
@@ -1,7 +1,5 @@
-import { useState, useEffect, Fragment } from "react";
+import { Fragment } from "react";
 import * as React from "react";
-import styled, { css } from "styled-components";
-import Typography from "@material-ui/core/Typography";
 import ButtonAppBar from "../components/ButtonAppBar";
 import { ParticipantSection } from "./ParticipantSection";
 
@@ -14,7 +12,6 @@ import {
   SectionContainer,
   CeremonyTitle
 } from "../styles";
-import { AuthStateContext } from "../state/AuthContext";
 import AddCeremonyPage from "./AddCeremony";
 import Modal from "@material-ui/core/Modal";
 import { CeremonyPage } from "./CeremonyPage";
@@ -22,7 +19,6 @@ import Footer from './../components/Footer';
 import AboutPanel from './../components/AboutPanel';
 import { useSelectionContext } from '../state/SelectionContext';
 import './styles.css';
-import { withStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import CircuitsPanel from "../components/CircuitsPanel";
 
@@ -38,41 +34,35 @@ export const LandingPage = () => {
     const closeModal = () => {dispatch({type: 'CLOSE_CEREMONY'});}
 
     return (
-        <AuthStateContext.Consumer>
-          {Auth => {
-            console.debug(`landing page: ${Auth.isCoordinator}`);
-            return (
-              <Fragment>
-                <ButtonAppBar />
-                <PageContainer>
-                  <div>
-                    <Box style={{ height: '608px' }} >
-                      <ParticipantSection />
-                    </Box>
-                    <Box style={{ height: '486px' }} >
-                      <AboutPanel />
-                    </Box>
-                    <CircuitsPanel />
-                    <Box style={{ height: '112px' }} >
-                      <Footer />
-                    </Box>
-                    <Modal
-                      open={selection.openModal}
-                      onClose={closeModal}
-                      aria-labelledby="simple-modal-title"
-                      aria-describedby="simple-modal-description"
-                    >
-                      {(selection.circuitDetail) ?
-                        (<CeremonyPage onClose={closeModal} />)
-                        : (selection.newCircuit) ? 
-                        (<AddCeremonyPage />)
-                        : (<></>)
-                      }
-                    </Modal>
-                  </div>
-                </PageContainer>
-              </Fragment>
-          )}}
-        </AuthStateContext.Consumer>
+      <Fragment>
+        <ButtonAppBar />
+        <PageContainer>
+          <div>
+            <Box style={{ height: '608px' }} >
+              <ParticipantSection />
+            </Box>
+            <Box style={{ height: '486px' }} >
+              <AboutPanel />
+            </Box>
+            <CircuitsPanel />
+            <Box style={{ height: '112px' }} >
+              <Footer />
+            </Box>
+            <Modal
+              open={selection.openModal}
+              onClose={closeModal}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+            >
+              {(selection.circuitDetail) ?
+                (<CeremonyPage onClose={closeModal} />)
+                : (selection.newCircuit) ? 
+                (<AddCeremonyPage />)
+                : (<></>)
+              }
+            </Modal>
+          </div>
+        </PageContainer>
+      </Fragment>
   );
 };
