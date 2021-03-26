2de4ca0c870fb42b7da3afd468fec02176c914a5
Chance Hudson <jchancehud@gmail.com>
2021-03-26 14:21:23 -0500

    refactor: about/feature section

--- client/src/components/AboutPanel.tsx
+++ client/src/components/AboutPanel.tsx
@@ -1,4 +1,3 @@
-import { Box, Button, Grid, Typography } from '@material-ui/core';
 import * as React from 'react';
 import { useContext, useState } from 'react';
 import { CalendarIcon, LightningIcon, PopOutIcon, ScaleIcon, SecureIcon, StarIcon, } from '../icons';
@@ -22,92 +21,74 @@ export default function AboutPanel(props: any) {
   }
 
   const leftPanel = (
-    <Grid container direction='column'>
-      <Grid item>
-        <PanelTitle style={{ marginBottom: '40px' }}>{`about ${project}`}</PanelTitle>
-      </Grid>
-      <Grid item>
-        <SubtleBody style={{ marginBottom: '121px', justifyContent: 'left' }}>
-          [zk-optimistic-rollup]
-        </SubtleBody>
-      </Grid>
-      <Grid item>
-        <NormalBodyText style={{ wordWrap: 'break-word', marginBottom: '40px' }}>
-          Zkopru is a novel layer-2 scaling solution that
-          supports private transfers and private atomic
-          swaps between ETH, ERC20, ERC721 at a low cost.
-        </NormalBodyText>
-      </Grid>
-      <Grid>
-        <StyledButton style={{ width: '217px', height: '53px', justifyContent: 'space-evenly' }}
-          onClick={openModal}
-        >
-          Read More
-          {PopOutIcon}
-        </StyledButton>
-      </Grid>
-    </Grid>
-  );
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <PanelTitle>{`about ${project}`}</PanelTitle>
+      <SubtleBody>
+        [zk-optimistic-rollup]
+      </SubtleBody>
+      <div style={{ height: '121px' }} />
+      <NormalBodyText style={{ wordWrap: 'break-word', marginBottom: '40px', maxWidth: '424px' }}>
+        Zkopru is a novel layer-2 scaling solution that
+        supports private transfers and private atomic
+        swaps between ETH, ERC20, ERC721 at a low cost.
+      </NormalBodyText>
+      <StyledButton style={{ width: '217px', height: '53px', justifyContent: 'space-evenly' }}
+        onClick={openModal}
+      >
+        Read More
+        {PopOutIcon}
+      </StyledButton>
+    </div>
+  )
 
   const Feature = (props: any) => {
     return (
-      <Grid item container direction='row' style={{ marginBottom: '42px' }}>
-        <Grid item style={{ marginLeft: '50px', marginRight: '36px' }}>
-          {props.icon}
-        </Grid>
-        <Grid item>
-          {props.children}
-        </Grid>
-      </Grid>
-    );
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '42px' }}>
+        <div>{props.icon}</div>
+        <div style={{ width: '36px' }} />
+        <div>{props.children}</div>
+      </div>
+    )
   }
 
   const featuresPanel = (
-    <Box>
-      <Grid container direction='column' >
-        <Grid item style={{ 
-            fontFamily: 'Inconsolata', 
-            fontSize: '24px', 
-            letterSpacing: '0.1em', 
-            color: subtleText, 
-            textTransform: 'uppercase',
-            borderBottom: '1px',
-            borderBottomColor: subtleText,
-            paddingLeft: '41px',
-            paddingRight: '51px',
-            paddingTop: '37px',
-          }}>
-          Features
-        </Grid>
-        <Feature icon={SecureIcon}>zkSNARK Privacy</Feature>
-        <Feature icon={ScaleIcon}>Optimistic Rollup Scaling</Feature>
-        <Feature icon={StarIcon}>Compliance Compatibility</Feature>
-        <Feature icon={LightningIcon}>Instant Withdrawals</Feature>
-        <Feature icon={(<PoolIcon />)}>Privacy Pool for ERC20/NFTs</Feature>
-      </Grid>
-    </Box>
-  );
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '47px',
+      paddingBottom: '0px',
+      backgroundColor: '#0E2936',
+      boxShadow: '3px 3px 0px #5D7078',
+    }}>
+      <div style={{ fontFamily: 'Inconsolata', fontSize: '24px', color: '#95A7AE', fontWeight: 'bold' }}>
+        FEATURES
+      </div>
+      <div style={{ height: '1px', backgroundColor: '#5D7078', marginTop: '6px', marginBottom: '48px' }} />
+      <Feature icon={SecureIcon}>zkSNARK Privacy</Feature>
+      <Feature icon={ScaleIcon}>Optimistic Rollup Scaling</Feature>
+      <Feature icon={StarIcon}>Compliance Compatibility</Feature>
+      <Feature icon={LightningIcon}>Instant Withdrawals</Feature>
+      <Feature icon={(<PoolIcon />)}>Privacy Pool for ERC20/NFTs</Feature>
+    </div>
+  )
 
-  const aboutModal = (modalOpen) ? 
-    <ViewLog 
-      open={modalOpen} 
-      close={closeModal} 
-      content={(<About isParticipant={authState.isLoggedIn} />)} 
+  const aboutModal = (modalOpen) ?
+    <ViewLog
+      open={modalOpen}
+      close={closeModal}
+      content={(<About isParticipant={authState.isLoggedIn} />)}
       title={`About the zkopru trusted setup`} />
   :
     (<></>);
 
   return (
-    <Box>
-      <Grid container direction='row'>
-        <Grid item style={{ width: '500px' }}>
-          {leftPanel}
-        </Grid>
-        <Grid item style={{ width: '407px', left: '859px', background: darkerBackground, marginTop: '50px' }}>
-          {featuresPanel}
-        </Grid>
-      </Grid>
+    <>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {leftPanel}
+        <div style={{ flex: 1, height: '1px', maxWidth: '264px' }} />
+        {featuresPanel}
+      </div>
       {aboutModal}
-    </Box>
+    </>
   );
 }
