003fc5689425652aea5c71b9505d2efff4125013
Geoff Lamperd <glamperd@gmail.com>
2021-06-16 19:09:47 +1000

    no web-worker in build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,26 +1,5 @@
 module.exports = {
   module: {
-    rules: [
-      { 
-        test: /\.m?js$/, 
-        include: [path.resolve('node_modules/ffjavascript/')],
-        exclude: /node_modules\/(?!ffjavascript).+/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime', 
-              {
-                  "absoluteRuntime": false,
-                  "corejs": false,
-                  "helpers": true,
-                  "regenerator": true,
-                  "version": "7.0.0-beta.0"
-              }],
-          }
-        }
-      },
-    ],
     resolve: {
         fallback: {
           util: require.resolve("util/")
