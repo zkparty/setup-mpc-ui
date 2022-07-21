6b7fb9394089a2cc076d28b4d90ab723efbe37b6
Nico Serrano <nnicosp@hotmail.com>
2022-07-21 17:57:19 -0500

    client(improve): mobile and desktop splitted

--- client/src/components/LoginPanel.tsx
+++ client/src/components/LoginPanel.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import {
   WelcomeTitle,
   SubtleBody,
@@ -9,17 +8,26 @@ import env from '../env';
 
 export default function LoginPanel(props: any) {
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const updateWindowDimensions = () => {
-    setWindowWidth(window.innerWidth);
+  let content = [
+    <WelcomeTitle key="title">{env.projectName}</WelcomeTitle>,
+    <SubtleBody key="subtitle" style={{ textAlign: 'center' }}>
+      {`Trusted Setup Ceremony`}
+    </SubtleBody>
+  ];
+  // mobile
+  if (window.innerWidth < 1023){
+    content.push(
+      <NormalBodyText key="mobile" style={{ textAlign: 'center', marginTop: '8px' }}>
+        {`The ceremony has to be performed in desktop due to computation constrains. Please open this website on a desktop browser`}
+      </NormalBodyText>);
+  // desktop
+  } else {
+    content.push(
+      <NormalBodyText key="desktop" style={{ display: window.innerWidth >= 1023 ? '' : 'none', textAlign: 'center', marginTop: '8px' }}>
+        {`Participate using your Ethereum wallet (preferred) or using your Github account`}
+      </NormalBodyText>,
+      <Login key="login"/>);
   }
-
-  useEffect(() => {
-    updateWindowDimensions();
-    window.addEventListener('resize', updateWindowDimensions);
-    return () => window.addEventListener('resize', updateWindowDimensions);
-  }, []);
 
   return (
   <div style={{
@@ -28,19 +36,6 @@ export default function LoginPanel(props: any) {
     alignItems: 'center',
     paddingTop: '20px'
     }} >
-    <WelcomeTitle>{env.projectName}</WelcomeTitle>
-    <div style={{ height: '41px' }} />
-    <SubtleBody style={{ textAlign: 'center' }}>
-      {`Trusted Setup Ceremony`}
-    </SubtleBody>
-    <NormalBodyText style={{ display: windowWidth < 1023 ? '' : 'none', textAlign: 'center', marginTop: '8px' }}>
-      {`The ceremony has to be performed in desktop due to computation constrains. Please open this website on a desktop browser`}
-    </NormalBodyText>
-    <NormalBodyText style={{ display: windowWidth > 1023 ? '' : 'none', textAlign: 'center', marginTop: '8px' }}>
-      {`Participate using your Ethereum wallet (preferred) or using your Github account`}
-    </NormalBodyText>
-    <div style={{ display: windowWidth > 1023 ? '' : 'none', }} >
-      <Login/>
-    </div>
+    {content}
   </div>);
 }
