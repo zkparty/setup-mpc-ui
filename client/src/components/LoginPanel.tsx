fbafd64f8c8a736d556ce99bab0b5fbe90043a4f
Nico Serrano <nnicosp@hotmail.com>
2022-07-21 20:00:49 -0500

    client(fix): small fixes and reordering

--- client/src/components/LoginPanel.tsx
+++ client/src/components/LoginPanel.tsx
@@ -1,15 +1,41 @@
-import {
-  WelcomeTitle,
-  SubtleBody,
-  NormalBodyText,
-} from "../styles";
-import Login from './Login';
+import { useState } from "react";
 import env from '../env';
+import Login from './Login';
+import { WelcomeTitle, SubtleBody, NormalBodyText } from "../styles";
 
 export default function LoginPanel(props: any) {
 
+  const [title, setTitle] = useState(env.projectName);
+  let [intervalTime, setIntervalTime] = useState(0);
+
+  const onClick = () => {
+    if (intervalTime === 0) {
+      const newIntervalTime = setInterval(() => {
+        setTitle(getRandomText());
+      }, 1000 / 12);
+      setIntervalTime(newIntervalTime);
+
+      setTimeout(() => {
+        clearInterval(newIntervalTime);
+        setIntervalTime(0);
+        if (Math.random() < 0.3) {
+          setTitle(env.projectName);
+        }
+      }, 500);
+    }
+  };
+
+  const getRandomText = () => {
+    let result = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < title.length; i++) {
+      result += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return result;
+  }
+
   let content = [
-    <WelcomeTitle key="title">{env.projectName}</WelcomeTitle>,
+    <WelcomeTitle key="title" onClick={onClick}>{title}</WelcomeTitle>,
     <SubtleBody key="subtitle" style={{ textAlign: 'center' }}>
       {`Trusted Setup Ceremony`}
     </SubtleBody>
--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import {
-  accentColor,
-  textColor,
-} from "../styles";
+import { textColor } from "../styles";
 import env from '../env';
 
 const LandingPageTitle = styled.div`
