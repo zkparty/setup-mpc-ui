b8c86f9d753b7a29df3e39fdc3a7c0cfa8e7918a
Nico Serrano <nnicosp@hotmail.com>
2022-07-29 23:40:44 -0400

    client(fix): housekeeping unused funcs and order

--- client/src/state/Compute.tsx
+++ client/src/state/Compute.tsx
@@ -1,73 +1,41 @@
-import * as React from 'react';
-import { getParamsFile, uploadParams } from "../api/FileApi";
-import { Ceremony, Project } from "../types/ceremony";
-
 import { Dispatch } from "react";
+
+import { Ceremony, Project } from "../types/ceremony";
+import { getParamsFile, uploadParams } from "../api/FileApi";
 import { getParticipantContributions } from '../api/FirestoreApi';
-//import { zKey, powersOfTau } from 'snarkjs';
 
 export const startDownload = (ceremonyId: string, index: number, prefix: string, suffix: string, dispatch: Dispatch<any>) => {
     // DATA DOWNLOAD
     console.debug(`getting data ${ceremonyId} ${index}`);
     const progressCb = (progress: number) => dispatch({type: 'PROGRESS_UPDATE', data: progress})
     getParamsFile(ceremonyId, index, prefix, suffix, progressCb).then( paramData => {
-        //setTimeout(() => {
-            // deep copy so params can be transfered to worker (even with multiple invocations of same function)
-            const paramsToTransferToWorker = new Uint8Array(paramData);
-            console.debug(`downloaded ${paramData?.length}`);
-            dispatch({
-                type: 'DOWNLOADED',
-                ceremonyId,
-                data: paramsToTransferToWorker,
-                dispatch,
-            });
-        //}, 500);
+        // deep copy so params can be transfered to worker (even with multiple invocations of same function)
+        const paramsToTransferToWorker = new Uint8Array(paramData);
+        console.debug(`downloaded ${paramData?.length}`);
+        dispatch({
+            type: 'DOWNLOADED',
+            ceremonyId,
+            data: paramsToTransferToWorker,
+            dispatch,
+        });
     }).catch(err => {
          console.error(`Error: ${err.message}. Skipping circuit`);
          // Failed download - abort and invalidate the contribution
          dispatch({type: 'ABORT_CIRCUIT', data: err.message, dispatch});
     });
 };
-enum ComputeMode {
-    ZKEY,
-    POWERSOFTAU,
-  };
 
 
 const PROGRESS_UPDATE = 'PROGRESS_UPDATE';
 export const startComputation = (params: Uint8Array, entropy: Uint8Array, participant: string,
     dispatch: Dispatch<any>, worker: Worker) => {
-
-    const progressOptions = {
-        progressCallback: (val: number, total: number) => {
-            //console.debug(`compute progress = ${val} of ${total}`);
-            dispatch({
-                type: PROGRESS_UPDATE,
-                data: total > 0 ? 100 * val / total : 0,
-            })
-        }
-    }
-    const inputFd = { type: 'mem', data: params };
-    let outFd =  { type: 'mem', data: new Uint8Array() };
-
-    const handleResult = (hash: any) => {
-        console.log(`contribution hash: ${JSON.stringify(hash)}`);
-        dispatch({type: 'SET_HASH', hash});
-        const result = outFd.data;
-        console.debug(`COMPLETE ${result.length}`);
-        dispatch({type: 'COMPUTE_DONE', newParams: result, dispatch });
-    }
-
     try {
         console.log(`params ${params.buffer.byteLength}`);
         const message = {
             type: 'COMPUTE',
             params: params.buffer
         };
-        worker.postMessage(message,
-            [
-                params.buffer
-            ]);
+        worker.postMessage(message, [params.buffer]);
         /*zKey.contribute( inputFd, outFd,
                 participant, entropy.buffer, console, progressOptions).then( */
           /*          (hash: any) => {
@@ -140,7 +108,7 @@ export const getEntropy = () => {
     return new Uint8Array(64).map(() => Math.random() * 256);
 };
 
-export const startWorkerThread = (dispatch: React.Dispatch<any>) => {
+export const startWorkerThread = (dispatch: Dispatch<any>) => {
     if (!dispatch) return;
 
     console.log(`CrossOriginIsolated? ${window.crossOriginIsolated}`);
